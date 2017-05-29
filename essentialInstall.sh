af62828fb9e16a9a0f111da663a36365a3ceebf1
Moein Owhadi Kareshk <owhadi@users.noreply.github.com>
2017-05-29 02:55:10 -0600

    Change the python section.

--- essentialInstall.sh
+++ essentialInstall.sh
@@ -23,7 +23,7 @@ apt-get install -y vim nano sublime-text-installer
 apt-get install -y texlive-full texmaker
 
 # C++
-apt-get install -y gcc g++ libboost-all-dev
+apt-get install -y clang gcc g++ libboost-all-dev
 
 # Ruby
 apt-get install -y ruby
@@ -37,13 +37,22 @@ apt-get install nodejs
 # Python 3
 apt-get install -y python3-dev python3-numpy python3-scipy python3-matplotlib python3-sympy python3-pandas python3-pip ipython3-notebook
 pip3 install --upgrade pip
-pip3 install cython request bs4 tweepy twython flask django py2app nltk spacy tqdm scikit-learn autobahn nose sqlalchemy keras
-
+pip3 install cython 
+pip3 install flask django py2app
+pip3 install request bs4 
+pip3 install tweepy twython
+pip3 install nltk spacy
+pip3 install scikit-learn keras
                      
 # Python 2
 apt-get install -y python-dev python-numpy python-scipy python-matplotlib python-sympy python-pandas python-pip ipython-notebook
 pip install --upgrade pip
-pip install cython request bs4 tweepy twython flask django py2app nltk spacy tqdm scikit-learn autobahn nose sqlalchemy keras
+pip3 install cython 
+pip3 install flask django py2app
+pip3 install request bs4 
+pip3 install tweepy twython
+pip3 install nltk spacy
+pip3 install scikit-learn keras
 
 # OpenCV
 apt-get install -y ffmpeg libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff5-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libtbb-dev libqt4-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils
