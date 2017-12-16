2c455599ff94aff8339cf167c30a62e1556d6870
Moein Owhadi-Kareshk <owhadi@users.noreply.github.com>
2017-12-15 23:26:02 -0700

    Update essentialInstall.sh

--- essentialInstall.sh
+++ essentialInstall.sh
@@ -36,18 +36,6 @@ apt-get install -y default-jdk* gradle maven openjdk-8-jdk openjfx
 # Node.js
 apt-get install -y nodejs 
 
-# Python 3
-apt-get install -y python3-dev python3-numpy python3-scipy python3-matplotlib python3-sympy python3-pandas python3-pip ipython3-notebook python-h5py
-3pip3 install --upgrade pip 
-pip3 install cython  
-pip3 install flask django py2app 
-pip3 install request bs4  
-pip3 install tweepy twython 
-pip3 install nltk spacy 
-pip3 install scikit-learn keras 
-pip3 install graphviz
-pip3 install joblib
-                     
 # Python 2
 apt-get install -y python-dev python-numpy python-scipy python-matplotlib python-sympy python-pandas python-pip ipython-notebook python-h5py
 pip install --upgrade pip 
@@ -60,7 +48,18 @@ pip install scikit-learn keras
 pip install graphviz
 pip install joblib
 
-
+# Python 3
+apt-get install -y python3-dev python3-numpy python3-scipy python3-matplotlib python3-sympy python3-pandas python3-pip ipython3-notebook python-h5py
+3pip3 install --upgrade pip 
+pip3 install cython  
+pip3 install flask django py2app 
+pip3 install request bs4  
+pip3 install tweepy twython 
+pip3 install nltk spacy 
+pip3 install scikit-learn keras 
+pip3 install graphviz
+pip3 install joblib
+                     
 # OpenCV
 apt-get install -y ffmpeg libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff5-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libtbb-dev libqt4-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils
 apt-get install -y libopencv-dev python-opencv  
