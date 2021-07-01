869dd7534f6471d67eeed59107f6e4e211acb5ac
Moein Kareshk <mkareshk@outlook.com>
2021-07-01 16:33:30 -0700

    update the ubuntu script, restructure

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
 # Update and Ungrade
-apt-get update 
-apt-get upgrade -y
+apt-get update && apt-get upgrade -y
 
 # Essential Tools
 apt-get install -y git ssh build-essential make cmake csh curl graphviz libgraphviz-dev screen tmux vim nano
 
-# Libs
-apt-get install -y libblas-dev liblapack-dev libatlas-base-dev 
-
 # Fortran
 apt-get install -y gfortran 
 
+# C++
+apt-get install -y clang gcc g++ 
+
+# Libs
+apt-get install -y libblas-dev liblapack-dev libatlas-base-dev libboost-all-dev 
+
 # LaTeX
 apt-get install -y texlive-full texmaker
-
-# C++
-apt-get install -y clang gcc g++ libboost-all-dev 
 
 # Ruby
 apt-get install -y ruby 
@@ -29,7 +28,7 @@ apt-get install -y default-jdk gradle maven openjfx
 apt-get install -y nodejs 
 
 # Python 3
-apt-get install -y python3-dev python3-pip python3-setuptools
+apt-get install -y python3-dev python3-pip python3-setuptools python3-wheel
 
 # Cleaning
 apt-get autoremove
